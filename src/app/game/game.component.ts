e13d7c1a81fecbaef437b5d8091f5807357cbf4c
oscarcederberg <os7138ce-s@student.lu.se>
2022-03-07 14:11:58 +0100

    Begun on CalculateWinner

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -11,6 +11,7 @@ export class GameComponent implements OnInit {
   width = 7;
 
   board!: number[];
+  board2d!: number[][];
   playerIsNext!: boolean;
   winner!: number | null;
 
@@ -23,6 +24,7 @@ export class GameComponent implements OnInit {
 
   newGame() {
     this.board = Array(this.height * this.width).fill(null);
+    this.board2d = Array(this.width).fill(Array(this.height).fill(0));
     this.winner = null;
     this.playerIsNext = true;
   }
@@ -41,9 +43,11 @@ export class GameComponent implements OnInit {
 
         if (cell) {
           this.board.splice(index - this.width, 1, this.player);
+          this.board2d[x][y - 1] = this.player;
           break;
         } else if (y == this.height - 1) {
           this.board.splice(index, 1, this.player);
+          this.board2d[x][y] = this.player;
           break;
         }
       }
@@ -55,6 +59,28 @@ export class GameComponent implements OnInit {
   }
 
   calculateWinner() {
+    for (let index = 0; index < this.board2d.length; index++) {
+      const element = this.board2d[index][0];
 
+      if (element == 0) {
+        break;
+      } else if (element != 0 && index == this.board2d.length - 1) {
+        this.winner = 0;
+      }
+    }
+  }
+
+  transpose(board: number[][]) {
+    let transposed = Array(board[0].length).fill(Array(board.length).fill(0));
+    
+    for(var i = 0; i < board.length; i++){
+        for(var j = 0; j < board[0].length; j++){
+            transposed[j][i] = board[i][j];
+        };
+    };
+  }
+  
+  flip(board: number[][]) {
+    let flipped = Array(board.length).fill(Array(board[0].length).fill(0));
   }
 }
