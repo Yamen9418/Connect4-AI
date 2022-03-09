a5c9a6e69e9aaa022026b74479e0548839894814
mo4718al-s <mo4718al-s@student.lu.se>
2022-03-09 13:54:37 +0100

    trying to solve merge conflict

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+// add the correct parameters of the Api in start
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  board: String = '';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+  ngOnInit(): void {
+    this.getState();
+    this.getMove1(3);
+
+  }
+  getState() {
+    this.httpClient.get<any>('http://localhost:8000/state').subscribe(
+      (response: String) => {
+        //console.log(typeof response);
+        this.board = response;
+        return response;
+
+      }
+    );
+  }
+
+  getMove1(col: number) {
+    this.httpClient.get<any>('http://localhost:8000/move1/' + col).subscribe(
+      response => {
+        this.board = response.split("Is_done").pop();
+        return response; //gör split här också
+
+
+      }
+    );
+  }
   title = 'EDAF90-Project';
 }
--- src/skeleton_code_and_gym_environment/skeleton.py
+++ src/skeleton_code_and_gym_environment/skeleton.py
@@ -1,3 +1,4 @@
+from fastapi import FastAPI
 import json
 import math
 import pprint
