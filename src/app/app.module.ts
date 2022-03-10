245e5ae923b38a057eaa52a118483a66d56af7d0
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-03-10 20:08:57 +0100

    Change UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -35,6 +37,8 @@ import { ModeFormComponent } from './mode-form/mode-form.component';
     MatToolbarModule,
     MatTabsModule,
     MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
     FormsModule, 
     ReactiveFormsModule
   ],
